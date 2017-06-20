cc9a3a128824ba26652a6207131a99b95e1a16ff
Akash <akash.darknight@gmail.com>
2017-06-20 08:03:00 +0530

    Estimating Gaussian Value

---
+++ estimateGaussian.m
@@ -0,0 +1,27 @@
+function [mu sigma2] = estimateGaussian(X)
+%ESTIMATEGAUSSIAN This function estimates the parameters of a 
+%Gaussian distribution using the data in X
+%   [mu sigma2] = estimateGaussian(X), 
+%   The input X is the dataset with each n-dimensional data point in one row
+%   The output is an n-dimensional vector mu, the mean of the data set
+%   and the variances sigma^2, an n x 1 vector
+% 
+
+% Useful variables
+[m, n] = size(X);
+
+%
+mu = zeros(n, 1);
+sigma2 = zeros(n, 1);
+
+
+mu1= mean(X);
+mu(:)=mu1(1,:);
+
+sigma_t= var(X,1);
+sigma2(:)=sigma_t(1,:);
+
+
+% ===========================================================
+
+end
